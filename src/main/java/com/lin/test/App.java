f7e972a2b0c7442dff0b4257f62c2b9b8e91de56
nianliner <876395565@qq.com>
2017-06-22 17:59:52 +0800

    nianlin update 2

--- src/main/java/com/lin/test/App.java
+++ src/main/java/com/lin/test/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "---nianlin update---" );
+        
+        System.out.println("---nianlin update 2---");
     }
 }
